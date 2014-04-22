fe8052822ce5487842ce72b6697defce81dbf532
jonesguy14 <jonesguy14@gmail.com>
2014-04-21 21:04:49 -0400

    Update expression.h

--- expression.h
+++ expression.h
@@ -7,53 +7,27 @@
 
 using namespace std;
 
-class Expression;
-class Fraction;
-class Logarithm;
-class Exponent;
-class Number;
-class Constant;
-class Polynomial;
-
-class Expression{
-private:
-	vector<Expression*> addition;
-	vector<Expression*> multiplication;
-	vector<Expression*> value;
-	bool initialized;
-
-public:
-	Expression(Expression*);
-	Expression(vector<Expression*>);
-	Expression();
-
-	virtual Expression* add(Expression* ex);
-	virtual Expression* subtract(Expression* ex);
-
-	virtual Expression* multiply(Expression* ex);
-	virtual Expression* divide(Expression* ex);
-
-	virtual Expression* exponentiate(Expression* ex);
-
-	virtual bool canAdd(Expression* ex);
-	virtual bool canSubtract(Expression* ex);
-
-	virtual bool canMultiply(Expression* ex);
-	virtual bool canDivide(Expression* ex);
-
-	virtual bool canExponentiate(Expression* ex);
-
-	virtual void negative();
-
-	virtual void simplify();
-	virtual void clear();
-	virtual bool empty();
-
-	virtual string toString();
-	virtual double toDecimal();
-	virtual string getName();
-
-	virtual Expression* getValue1();
+class Expression {
+private:    
+    vector<Expression*> expression;
+    vector<Expression*> numerator;
+    vector<Expression*> denominator;
+    
+public: 
+    virtual Expression* add(Expression* ex) = 0;
+    virtual Expression* subtract(Expression* ex) = 0;
+    virtual Expression* multiply(Expression* ex) = 0;
+    virtual Expression* divide(Expression* ex) = 0;
+    
+    virtual Expression* simplify() = 0;
+    
+    virtual string toString() = 0;
+    virtual double toDecimal() = 0;
+    virtual string getName() = 0;
+    
 };
 
 #endif
+    
+    
+    
