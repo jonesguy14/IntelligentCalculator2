59a02a6a984722eca70cf7e857af7c655a9ee1b0
jonesguy14 <jonesguy14@gmail.com>
2014-04-28 15:12:56 -0400

    Update MultVecEx.h

--- MultVecEx.h
+++ MultVecEx.h
@@ -1,5 +1,6 @@
-#ifndef MultVecEx_H_
-#define MultVecEx_H_
+#ifndef MULTVECEX_H_INCLUDED
+#define MULTVECEX_H_INCLUDED
+
 #include <iostream>
 #include "expression.h"
 
@@ -7,29 +8,32 @@ class MultVecEx : public Expression {
 private:
     vector<Expression*> numerator;
     vector<Expression*> denominator;
-    
+
 public:
     MultVecEx();
     MultVecEx(Expression* numerator, Expression* denominator);
     MultVecEx(vector<Expression*> numerator, vector<Expression*> denominator);
     MultVecEx(vector<Expression*> numerator);
-    
+
     Expression* add(Expression* ex);
-    //Expression* subtract(Expression* ex);
+    Expression* subtract(Expression* ex);
     Expression* multiply(Expression* ex);
     Expression* divide(Expression* ex);
-    
+
     Expression* simplify();
-    
+
     string toString();
     string getName();
     double toDecimal();
-    
+
+    void negative();
+
     vector<Expression*> simplifyVec();
     int GreatCommonFactor(int a, int b);
-    
+
     vector<Expression*> getNumerator();
     vector<Expression*> getDenominator();
-    
+
 };
-    
+
+#endif // MULTVECEX_H_INCLUDED
