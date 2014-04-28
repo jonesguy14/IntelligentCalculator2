ba6e05992ca6820f43a2aac2fab26080c0738f69
jonesguy14 <jonesguy14@gmail.com>
2014-04-28 15:10:14 -0400

    Update expression.h

--- expression.h
+++ expression.h
@@ -8,26 +8,27 @@
 using namespace std;
 
 class Expression {
-private:    
+private:
     vector<Expression*> expression;
     vector<Expression*> numerator;
     vector<Expression*> denominator;
-    
-public: 
+    char sign;
+
+public:
     virtual Expression* add(Expression* ex) = 0;
     virtual Expression* subtract(Expression* ex) = 0;
     virtual Expression* multiply(Expression* ex) = 0;
     virtual Expression* divide(Expression* ex) = 0;
-    
+
     virtual Expression* simplify() = 0;
-    
+
+    void negative();
+    char getSign();
+
     virtual string toString() = 0;
     virtual double toDecimal() = 0;
     virtual string getName() = 0;
-    
+
 };
 
 #endif
-    
-    
-    
