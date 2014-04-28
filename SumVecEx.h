c4d81bb45e05845659d9de39e66edde4cf498f06
jonesguy14 <jonesguy14@gmail.com>
2014-04-28 15:11:30 -0400

    Update SumVecEx.h

--- SumVecEx.h
+++ SumVecEx.h
@@ -1,30 +1,34 @@
-#ifndef SumVecEx_H_
-#define SumVecEx_H_
+#ifndef SUMEXVEC_H_INCLUDED
+#define SUMEXVEC_H_INCLUDED
 #include <iostream>
 #include "expression.h"
 
 class SumVecEx : public Expression {
 private:
     vector<Expression*> expression;
-    
+
 public:
     SumVecEx();
     SumVecEx(Expression* e1, Expression* e2);
     SumVecEx(vector<Expression*> e);
-    
+
     Expression* add(Expression* ex);
-    //Expression* subtract(Expression* ex);
+    Expression* subtract(Expression* ex);
     Expression* multiply(Expression* ex);
     Expression* divide(Expression* ex);
-    
+
     Expression* simplify();
-    
+
     string toString();
     string getName();
     double toDecimal();
-    
-    vector<Expression*> simplifyVec();    
+
+    void negative();
+
+    vector<Expression*> simplifyVec();
     vector<Expression*> getExpression();
-    
+
 };
-    
+
+
+#endif // SUMEXVEC_H_INCLUDED
