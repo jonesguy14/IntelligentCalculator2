59299dae4849ecd255c2e784b1ebbc2dc62aecc1
jamesmsteele <jamessteele90@gmail.com>
2014-04-28 20:11:42 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,32 +1,25 @@
 #include <iostream>
 #include <stdio.h>
+
 #include <vector>
 #include "shuntingyard.h"
 
 using namespace std;
 
 int main(){
-	vector<Expression*> expressions;
-
-	Number n1(2145.451);
-	Number n2(2145.451);
-	Constant pi("pi");
-	Constant pi2("pi");
-
-	expressions.push_back(pi.multiply(&pi2));
-
-	expressions.push_back(expressions[0]->subtract(&n2));
-	expressions.push_back(expressions[1]->add(&n2));
-	expressions.push_back(expressions[2]->multiply(&pi));
-	cout << expressions.back()->toString() << endl;
-
-	/*
 	try{
-		//ShuntingYard s1("3 + 4 * 2 / ( 1 - 5 ) ^ 2 ^ 3");
-		//cout << endl;
-		//ShuntingYard s2("((((log_5:((log_5:9 + 6 + 10) * 6))))) + 4 * 8 + e^(log_e:(5+6))/10 - sqrt:49 * 43rt:343 + pi * log_(log_55:5):(log_6:6)");
-	}catch(const char* e){
+
+		ShuntingYard s1("2/5 + 3/5");
+		cout << s1.getResult()->toString();
+		/*ShuntingYard s2("(3 + 2*pi) * (3 - 2*pi)");
+		cout << endl;
+		ShuntingYard s3("2^(5/3 + pi) / (3rt:4)");
+		cout << endl;
+		ShuntingYard s4("2^(5/3 + pi) / ((3rt:4) * 2^pi*5)");
+		cout << endl;
+		ShuntingYard s5("((((log_5:((log_5:9 + 6 + 10) * 6))))) + 4 * 8 + e^(log_e:(5+6))/10 - sqrt:49 * 43rt:343 + pi * log_(log_55:5):(log_6:6)");
+	*/}catch(const char* e){
 		cout << e;
 	}
-	*/
 }
+
