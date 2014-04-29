488a0a308335d63702c6529215af3040bed59f55
jamesmsteele <jamessteele90@gmail.com>
2014-04-28 21:29:20 -0400

    Update constant.cpp

--- constant.cpp
+++ constant.cpp
@@ -11,24 +11,32 @@ Constant::Constant(std::string value, Expression* coeff) {
     this->coefficient.push_back(coeff);
 }
 
-Expression* Constant::add(Expression* value){
-	if(value->getName() == "Constant"){
-        Constant* valc = static_cast<Constant*>(value);
-        Expression* temp	=	valc->getCoeff();
-		this->coefficient.push_back(this->coefficient.back()->add(temp));
-		return this;
+Expression* Constant::add(Expression* ex){
+	if(ex->getName() == "Constant"){
+        Constant* val		=	static_cast<Constant*>(ex);
+		if(this->getType() == val->getType()){
+			Constant* temp	=	new Constant(this->getType(), this->getCoeff()->add(val->getCoeff()));
+			return temp;
+		}else{
+			SummationVector* sum = new SummationVector(this, ex);
+			return sum;
+		}
 	}else{
-		SummationVector* sum = new SummationVector(this, value);
+		SummationVector* sum = new SummationVector(this, ex);
 		return sum;
 	}
 }
 
 Expression* Constant::subtract(Expression* ex){
 	if(ex->getName() == "Constant"){
-        Constant* valc = static_cast<Constant*>(ex);
-		Expression* temp	=	valc->getCoeff();
-		this->coefficient.push_back(this->coefficient.back()->subtract(temp));
-		return this;
+        Constant* val		=	static_cast<Constant*>(ex);
+		if(this->getType() == val->getType()){
+			Constant* temp	=	new Constant(this->getType(), this->getCoeff()->subtract(val->getCoeff()));
+			return temp;
+		}else{
+			SummationVector* sum = new SummationVector(this, ex->negative());
+			return sum;
+		}
 	}else{
 		SummationVector* sum = new SummationVector(this, ex->negative());
 		return sum;
@@ -36,64 +44,46 @@ Expression* Constant::subtract(Expression* ex){
 }
 
 Expression* Constant::multiply(Expression* value){
-    Number* one = new Number(1);
-	MultiplicationVector* multy = new MultiplicationVector(this, one);
-	Expression* ex = multy->multiply(value);
-	return ex;
+	if(value->getName() == "Number"){
+        Number* val		=	static_cast<Number*>(value);
+        Constant* temp	=	new Constant(this->value, this->getCoeff()->multiply(val));
+        return temp;
+	}else{
+		vector<Expression*> temp_vector;
+		temp_vector.push_back(this);
+		temp_vector.push_back(value);
+		MultiplicationVector* temp	=	new MultiplicationVector(temp_vector);
+		return temp;
+	}
 }
 
 Expression* Constant::divide(Expression* value){
-	Number* one = new Number(1);
-	MultiplicationVector* multy = new MultiplicationVector(this, one);
-	Expression* ex = multy->divide(value);
-	return ex;
-}
-
-bool Constant::canAdd(Expression* ex){
-    if (ex->getName() == "Constant") {
-        Constant* exc = static_cast<Constant*>(ex);
-        if (exc->getType()==this->value) {return true;}
-        else {return false;}
-    }
-	else {return false;}
-}
-
-bool Constant::canSubtract(Expression* ex){
-	if (ex->getName() == "Constant") {
-        Constant* exc = static_cast<Constant*>(ex);
-        if (exc->getType()==this->value) {return true;}
-        else {return false;}
-    }
-	else {return false;}
-}
-
-bool Constant::canMultiply(Expression* value){
-	return false;
-}
-
-bool Constant::canDivide(Expression* value){
-	return false;
-}
-
-bool Constant::canExponentiate(Expression* value){
-	return false;
+	if(value->getName() == "Number"){
+        Number* val		=	static_cast<Number*>(value);
+        MultiplicationVector* temp	=	new MultiplicationVector(this, val);
+        return temp;
+	}
+	if(value->getName() == "Constant"){
+        Constant* val		=	static_cast<Constant*>(value);
+		if(this->getType() == val->getType()){
+			MultiplicationVector* temp	=	new MultiplicationVector(this->getCoeff(), val->getCoeff());
+			return temp;
+		}
+	}else{
+		MultiplicationVector* temp	=	new MultiplicationVector(this, value);
+		return temp;
+	}
 }
 
 Expression* Constant::negative(){
-    Expression* newco = getCoeff()->negative();
-    Constant* negation = new Constant(value, newco);
+    Constant* negation = new Constant(this->value, getCoeff()->negative());
     return negation;
 }
 
 Expression* Constant::simplify(){
 	return this;
 }
-void Constant::clear(){
-	this->coefficient.clear();
-}
-bool Constant::empty(){
-	return (this->coefficient.empty());
-}
+
 
 std::string Constant::toString(){
 	if(this->getCoeff()->toDecimal() == 0){
